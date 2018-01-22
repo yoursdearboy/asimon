de15c318fabc6b5f6364f783e89612abca2dab78
Kirill Voronin <yoursdearboy@gmail.com>
2018-01-22 17:32:32 +0300

    R Package
    * Switch from CMake to R toolchain + autotools;
    * Minor fixes;

--- R/asimon.R
+++ R/asimon.R
@@ -1,21 +1,23 @@
-library(clinfun)
-dyn.load('cmake-build-release/asimon')
+.onLoad <- function(lib, pkg) {
+  library.dynam('asimon', pkg, lib)
+}
 
 g <- function(s1, r1, n1, s, m, r, n, p) {
-    .Call('g_facade', s1, r1, n1, s, m, r, n, p)
+    .Call('g_facade', s1, r1, n1, s, m, r, n, p, PACKAGE = packageName())
 }
 
 en <- function(s1, r1, n1, m2, n2, p) {
-    .Call('en_facade', s1, r1, n1, m2, n2, p)
+    .Call('en_facade', s1, r1, n1, m2, n2, p, PACKAGE = packageName())
 }
 
 asimon_calculate <- function(n1, n2, a_max, b1_max, b2_max, p0, p1, p2) {
-    .Call('asimon_facade', n1, n2, a_max, b1_max, b2_max, p0, p1, p2)
+    .Call('asimon_facade', n1, n2, a_max, b1_max, b2_max, p0, p1, p2, PACKAGE = packageName())
 }
 
+#' @export
 asimon <- function(a_max, b1_max, b2_max, p0, p1, p2) {
-    simon.n1 <- ph2simon(p0, p1, a_max, b1_max)
-    simon.n2 <- ph2simon(p0, p2, a_max, b2_max)
+    simon.n1 <- clinfun::ph2simon(p0, p1, a_max, b1_max)
+    simon.n2 <- clinfun::ph2simon(p0, p2, a_max, b2_max)
     n1 <- simon.n1$out[which.min(simon.n1$out[,5]),4]
     n2 <- simon.n2$out[which.min(simon.n2$out[,5]),4]
     x <- asimon_calculate(n1, n2, a_max, b1_max, b2_max, p0, p1, p2)
@@ -28,7 +30,7 @@ asimon <- function(a_max, b1_max, b2_max, p0, p1, p2) {
 
 print.asimon <- function(x, ...) {
     out <- do.call(rbind, lapply(x, function(r) r[1,]))
-    rownames(out) <- rep('OT', nrow(out))
+    rownames(out) <- paste('OT', 1:nrow(out), sep = '')
     print(out, ...)
 }
 
